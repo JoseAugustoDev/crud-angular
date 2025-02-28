65956eca3b8c65eccdb594656b699a8ff44fcc1a
Jose Augusto Costa <joseaugustodeveloper@gmail.com>
2025-02-28 20:59:23 -0300

    Adicionando o spinner e estilizando a página de cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,25 +1,29 @@
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
+import { CommonModule } from '@angular/common';
+
+import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { CoursesService } from '../services/courses.service';
-import { Observable } from 'rxjs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 @Component({
   selector: 'app-courses',
-  imports: [MatTableModule, MatCardModule, MatToolbarModule],
+  imports: [MatTableModule, MatCardModule, MatToolbarModule, MatProgressSpinnerModule, CommonModule],
   templateUrl: './courses.component.html',
   styleUrl: '/node_modules/bootstrap/dist/css/bootstrap.css'
 })
 export class CoursesComponent {
 
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
 
   constructor(private coursesService: CoursesService) {
 
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
   }
 
 
