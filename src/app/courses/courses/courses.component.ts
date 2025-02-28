c07872f4be53aa5caae6fa02ac600ed24a0c048b
Jose Augusto Costa <joseaugustodeveloper@gmail.com>
2025-02-28 14:51:16 -0300

    Adicionando os cards dos cursos. Lendo os cursos e colocando na tela

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { Course } from '../model/course';
+
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [MatTableModule, MatCardModule],
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: '../../../../node_modules/bootstrap/dist/css/bootstrap.css'
 })
 export class CoursesComponent {
+
+  courses: Course[];
+
+  constructor() {
+    this.courses = [
+      { _id: "1", name: "Curso de Angular", category: "Front-end" },
+      { _id: "2", name: "Curso de React", category: "Front-end" },
+      { _id: "3", name: "Curso de Vue.js", category: "Front-end" },
+      { _id: "4", name: "Curso de Node.js", category: "Back-end" },
+      { _id: "5", name: "Curso de Python", category: "Back-end" },
+      { _id: "6", name: "Curso de Java", category: "Back-end" },
+      { _id: "7", name: "Curso de Banco de Dados", category: "Banco de Dados" },
+      { _id: "8", name: "Curso de DevOps", category: "Infraestrutura" },
+      { _id: "9", name: "Curso de UI/UX Design", category: "Design" },
+      { _id: "10", name: "Curso de Cibersegurança", category: "Segurança" }
+    ];
+  }
+
 
 }
