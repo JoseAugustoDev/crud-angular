68ddc0b086df8280168d6325de42153dc067b64c
Jose Augusto Costa <joseaugustodeveloper@gmail.com>
2025-02-28 16:56:10 -0300

    Criação da Requisição HTTP, utilizando o método GET. Pegando as informações de um arquivo json

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -5,17 +5,17 @@ import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
   imports: [MatTableModule, MatCardModule, MatToolbarModule],
   templateUrl: './courses.component.html',
-  styleUrl: '../../../../node_modules/bootstrap/dist/css/bootstrap.css'
+  styleUrl: '/node_modules/bootstrap/dist/css/bootstrap.css'
 })
 export class CoursesComponent {
 
-  courses: Course[] = [];
-
+  courses: Observable<Course[]>;
 
   constructor(private coursesService: CoursesService) {
 
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,22 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor(private http: HttpClient) { }
+  private readonly API = '/assets/courses.json';
 
-  list(): Course[] {
-    return [
-      { _id: "1", name: "Curso de Angular", category: "Front-end" },
-      { _id: "2", name: "Curso de React", category: "Front-end" },
-      { _id: "3", name: "Curso de Vue.js", category: "Front-end" },
-      { _id: "4", name: "Curso de Node.js", category: "Back-end" },
-      { _id: "5", name: "Curso de Python", category: "Back-end" },
-      { _id: "6", name: "Curso de Java", category: "Back-end" }
-    ];
+  constructor(private httpClient: HttpClient) { }
+
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      tap(courses => console.log(courses))
+    );
   }
 }
