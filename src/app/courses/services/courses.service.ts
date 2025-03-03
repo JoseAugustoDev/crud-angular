bec73f9f8466594906fe0f35300bd253f1eca479
Jose Augusto Costa <joseaugustodeveloper@gmail.com>
2025-03-03 20:16:27 -0300

    Removendo imports não utilizados

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
-import { delay, first, tap } from 'rxjs';
+import { delay, first } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
